4ce141dc2c5818698d4f6fa10c44ecfb55d9d5f0
Ed Osborne <edosborne2001@hotmail.com>
2016-02-16 03:50:40 +0000

    Update test helloWorld. GitHub test

--- src/oata/HelloWorld.java
+++ src/oata/HelloWorld.java
@@ -3,5 +3,6 @@ package oata;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World. Jenkins test");
+	System.out.println("Hello World. Git Hub test");
     }
 }
